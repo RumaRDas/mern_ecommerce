060694516bfe3c8f21772584132d08fcab3540ea
Ruma Das <rumaadl16@gmail.com>
2020-09-07 20:19:51 +0930

    adding routes folder with user routes

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 require("dotenv").config();
-
+//import routes
+const userRoutes = require("./routes/user");
 //app
 const app = express();
 
@@ -14,11 +15,8 @@ mongoose
   })
   .then(() => console.log("Database Conntcted"));
 
-//routes
-app.get("/", (req, res) => {
-  res.send("Hello from node.....");
-});
-
+//routes middleware
+app.use("/api", userRoutes);
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
