28a903a15ecbdb2f465bd0f3130a61bb45c4b895
Ruma Das <rumaadl16@gmail.com>
2020-09-07 19:23:12 +0930

    adding database connrction

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+
 require("dotenv").config();
 
+//app
+const app = express();
+
+//database
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("Database Conntcted"));
+
+//routes
 app.get("/", (req, res) => {
-  res.send("hello from node.....");
+  res.send("Hello from node.....");
 });
 
 const port = process.env.PORT || 8000;
