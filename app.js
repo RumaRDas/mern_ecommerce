b87246f86b00d7808fe53e0fcc84e0aad29552be
Ruma Das <rumaadl16@gmail.com>
2020-09-08 10:41:53 +0930

    adding validator error msg

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
-
+const cookiParser = require("cookie-parser");
+const cors = require("cors");
+const expressValidator = require("express-validator");
 require("dotenv").config();
+
 //import routes
 const userRoutes = require("./routes/user");
 //app
@@ -20,6 +23,10 @@ mongoose
 //middlewares
 app.use(morgan("dev"));
 app.use(bodyParser.json());
+app.use(cookiParser());
+app.use(expressValidator());
+app.use(cors());
+
 //routes middleware
 app.use("/api", userRoutes);
 const port = process.env.PORT || 8000;
