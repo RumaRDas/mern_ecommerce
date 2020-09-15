4efa2903f6818fe11d36cd35aff572d37b8f591d
Ruma Das <rumaadl16@gmail.com>
2020-09-15 11:13:19 +0930

    including error massage when creating product

--- controllers/product.js
+++ controllers/product.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const Product = require("../models/product");
 
 exports.create = (req, res) => {
-  let form = new formidable.IncomingForm();
+  let form = new formidable.IncomingForm(); // comes with formidable packages
   form.keepExtensions = true;
   form.parse(req, (err, fields, files) => {
     if (err) {
@@ -12,8 +12,32 @@ exports.create = (req, res) => {
         error: "Image could not be uploaded",
       });
     }
+    // check for all fields
+    const { name, description, price, category, quantity, shipping } = fields;
+    if (
+      !name ||
+      !description ||
+      !price ||
+      !category ||
+      !quantity ||
+      !shipping
+    ) {
+      return res.status(400).json({
+        error: "ALL Fields are required",
+      });
+    }
     let product = new Product(fields);
+
+    // 1kb= 1000
+    // imb = 1000000
+
     if (files.photo) {
+      //console.log("FILES PHOTO: ", files.photo);
+      if (files.photo.size > 100000) {
+        return res.status(400).json({
+          error: "Image should be less then 1mb in size",
+        });
+      }
       product.photo.data = fs.readFileSync(files.photo.path);
       product.photo.contentType = files.photo.type;
     }
